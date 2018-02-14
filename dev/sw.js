f58f4348f8acebdababcf4e23c61783b3e7bfc26
ChapelR <Mason_718@hotmail.com>
2018-02-14 04:17:43 -0500

    trick

--- dev/sw.js
+++ dev/sw.js
@@ -1,25 +1,3 @@
 // to please lord google
-self.addEventListener( 'install', function (e) {
-    
-    function onInstall () {
-        return caches.open('static').then(function (cache) {
-            cache.add('index.html');
-        });
-    }
-    
-    e.waitUntil(onInstall(e))
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+self.addEventListener('install', function(event) {});
+self.addEventListener('fetch', function(event) {});
