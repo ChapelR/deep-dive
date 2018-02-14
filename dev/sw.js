df0bd9a9d5eadcc54c155b07fabfc7204d6b40e8
ChapelR <Mason_718@hotmail.com>
2018-02-14 02:33:08 -0500

    d

--- dev/sw.js
+++ dev/sw.js
@@ -123,22 +123,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log(urlsToCache);
         return cache.addAll(urlsToCache);
       })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
   );
 });
