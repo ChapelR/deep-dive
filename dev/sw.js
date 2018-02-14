9b564de893b9400d0af887f059612e3d3ba4e0a3
ChapelR <Mason_718@hotmail.com>
2018-02-14 03:02:59 -0500

    hmm

--- dev/sw.js
+++ dev/sw.js
@@ -129,3 +129,11 @@ self.addEventListener('install', function(event) {
         })
     );
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
